a568e0db9dc8b9ee7fb557583e002bf513065432
Roman Nahryshko <nagrishkoo@gmail.com>
2020-10-07 18:03:57 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -29,7 +29,10 @@ function getQuantityElements(heightElement){
 
 function startGame(){
     start.classList.add('hide');
-
+    gameArea.innerHTML = '';
+    car.style.left = '125px';
+    car.style.top = 'auto';
+    car.style.bottom = '10px'
     for (let i = 0; i < getQuantityElements(100); i++){
         const line = document.createElement('div')
         line.classList.add('line');
@@ -47,7 +50,7 @@ for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++){
     gameArea.appendChild(enemy);
 }
 
-
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
     setting.x = car.offsetLeft;
@@ -62,6 +65,8 @@ for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++){
 function playGame() {
 
     if (setting.start) {
+        setting.score += setting.speed;
+        score.innerHTML = 'SCORE<br>' +setting.score;
         moveRoad();
         moveEnemy();
         if (keys.ArrowLeft && setting.x > 0){
@@ -110,6 +115,18 @@ function moveRoad(){
 function moveEnemy(){
     let enemy = document.querySelectorAll('.enemy');
     enemy.forEach(function(item){
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+
+if(carRect.top <= enemyRect.bottom && 
+    carRect.right >= enemyRect.left &&
+    carRect.left <= enemyRect.right &&
+    carRect.bottom >= enemyRect.top){
+        setting.start = false;
+        start.classList.remove('.hide');
+        start.style.top = score.offsetHeight;
+}
+
         item.y += setting.speed/2;
         item.style.top = item.y + 'px';
         if(item.y >= document.documentElement.clientHeight){
